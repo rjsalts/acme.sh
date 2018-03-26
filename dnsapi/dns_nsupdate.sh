5b885d9500dd0b2e71c6644be118d88738bc763e
ACME updater <acme@red.spectralmud.org>
2018-03-27 00:22:33 +0200

    Add kerberos support to nsupdate dns api handler

--- dnsapi/dns_nsupdate.sh
+++ dnsapi/dns_nsupdate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+set -x
 ########  Public functions #####################
 
 #Usage: dns_nsupdate_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
@@ -7,13 +7,11 @@ dns_nsupdate_add() {
   fulldomain=$1
   txtvalue=$2
   _checkKeyFile || return 1
-  [ -n "${NSUPDATE_SERVER}" ] || NSUPDATE_SERVER="localhost"
-  # save the dns server and key to the account conf file.
-  _saveaccountconf NSUPDATE_SERVER "${NSUPDATE_SERVER}"
+  # save the TSIG key and type to the account conf file.
   _saveaccountconf NSUPDATE_KEY "${NSUPDATE_KEY}"
+  _saveaccountconf NSUPDATE_COMMAND "${NSUPDATE_COMMMAND}"
   _info "adding ${fulldomain}. 60 in txt \"${txtvalue}\""
-  nsupdate -k "${NSUPDATE_KEY}" <<EOF
-server ${NSUPDATE_SERVER}
+  "${NSUPDATE_COMMAND}" <<EOF
 update add ${fulldomain}. 60 in txt "${txtvalue}"
 send
 EOF
@@ -29,10 +27,8 @@ EOF
 dns_nsupdate_rm() {
   fulldomain=$1
   _checkKeyFile || return 1
-  [ -n "${NSUPDATE_SERVER}" ] || NSUPDATE_SERVER="localhost"
   _info "removing ${fulldomain}. txt"
-  nsupdate -k "${NSUPDATE_KEY}" <<EOF
-server ${NSUPDATE_SERVER}
+  "${NSUPDATE_COMMAND}" <<EOF
 update delete ${fulldomain}. txt
 send
 EOF
@@ -54,5 +50,29 @@ _checkKeyFile() {
   if [ ! -r "${NSUPDATE_KEY}" ]; then
     _err "key ${NSUPDATE_KEY} is unreadable"
     return 1
+  fi
+  if [ -z "${NSUPDATE_COMMAND}" ]; then
+    if _exists "file"; then
+      if file "${NSUPDATE_KEY}" | grep -q "Kerberos";then
+        NSUPDATE_COMMAND="nsupdate -g"
+      else
+        NSUPDATE_COMMAND="nsupdate -k ${NSUPDATE_KEY}"
+      fi
+    else
+      _err "Can't determine ${NSUPDATE_KEY} type."
+      _err "Install file, or set NSUPDATE_COMMAND."
+      return 1
+    fi
+  fi
+  if _endswith "${NSUPDATE_COMMAND}" "-g"; then
+    if _exists kinit; then
+      if ! kinit -k -t "${NSUPDATE_KEY}"; then
+        _err "Couldn't acquire kerberos ticket."
+        return 1
+      fi
+    else
+      _err "kinit not found."
+      return 1
+    fi
   fi
 }
